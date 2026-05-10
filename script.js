dd6b7258cc0161fda4520499740356bd5c141f78
hridoy2222 <59082827+hridoy2222@users.noreply.github.com>
2026-05-10 23:05:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+```javascript
+function changePair(pair,name){
+
+  document.getElementById("chartFrame").src =
+  `https://s.tradingview.com/widgetembed/?symbol=FX:${pair}&interval=1&theme=dark`;
+
+  document.getElementById("currentPair").innerText =
+  name;
+
+}
+
+/* TIMER */
+
+let time = 45;
+
+setInterval(() => {
+
+  let minutes =
+  String(Math.floor(time / 60)).padStart(2,'0');
+
+  let seconds =
+  String(time % 60).padStart(2,'0');
+
+  document.getElementById("countdown").innerText =
+  `${minutes}:${seconds}`;
+
+  if(time <= 0){
+    time = 45;
+  }else{
+    time--;
+  }
+
+},1000);
+```
